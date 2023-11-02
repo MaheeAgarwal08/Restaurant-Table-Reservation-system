2e98e12812ef372c34e8c1bed99104eccda600ed
Mahee Agarwal <agarwalmahee08@gmail.com>
2023-11-02 11:00:50 -0700

    third commit

--- bookTable.php
+++ bookTable.php
@@ -2,8 +2,52 @@
 <html>
 <head>
     <title>Table Reservation System</title>
+    <style>
+        /* Add these styles to your existing CSS or create a new CSS file */
+
+.booking-form {
+  background-color: #f9f9f9;
+  padding: 40px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  margin: 20px 0;
+}
+
+.booking-form h2 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.booking-form form {
+  display: flex;
+  flex-direction: column;
+}
+
+.booking-form label {
+  margin-bottom: 5px;
+}
+
+.booking-form input {
+  padding: 8px;
+  margin-bottom: 10px;
+}
+
+.booking-form button {
+  padding: 10px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.booking-form button:hover {
+  background-color: #0056b3;
+}
+</style>
 </head>
 <body>
+    <section class="booking-form">
 <h1>BOOK TABLE ACCORDING TO YOUR NEED AND CHOICE</h1>
 <h2>Check Table Availability</h2>
     <form action="check_availability.php" method="post">
@@ -26,6 +70,6 @@
         <input type="submit" value="Reserve Table">
     </form>
     <h6>Thanks for booking a .Hope you will have a great time with your family and friends</h6>
-   
+</section>
 </body>
 </html>
--- check_availability.php
+++ check_availability.php
@@ -1,6 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Table Availability</title>
+    <style>
+        /* Style the table */
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+
+
+
 <?php
 // Establish connection to the database
-$servername = "localhost";
+$servername="localhost";
 $username = "root";
 $password = "";
 $dbname = "table_reservations";
@@ -13,28 +38,41 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve form data
-$reservation_date = $_POST['reservation_date'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $reservation_date = $_POST['reservation_date'];
 
-// SQL to check table availability for the given date
-$sql = "SELECT t.table_number, t.capacity
-        FROM tables t
-        WHERE t.table_number NOT IN (
-            SELECT r.table_number
-            FROM reservations r
-            WHERE r.reservation_date = '$reservation_date'
-        )";
+    // SQL to check table availability for the given date
+    $sql = "SELECT t.table_number, t.capacity
+            FROM tables t
+            WHERE t.table_number NOT IN (
+                SELECT r.table_number
+                FROM reservations r
+                WHERE r.reservation_date = '$reservation_date'
+            )";
 
-$result = $conn->query($sql);
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    echo "Tables available for $reservation_date: <br>";
-    while ($row = $result->fetch_assoc()) {
-        echo "Table " . $row["table_number"] . " - Capacity: " . $row["capacity"] . " persons" .  "<br>";
+    if ($result->num_rows > 0) {
+        echo "<h2>Tables available for $reservation_date:</h2>";
+        echo "<table>";
+        echo "<tr><th>Table Number</th><th>Capacity</th></tr>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["table_number"] . "</td>";
+            echo "<td>" . $row["capacity"] . " persons</td>";
+           
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "<p>No tables available for $reservation_date</p>";
     }
-} else {
-    echo "No tables available for $reservation_date";
 }
 
 $conn->close();
 ?>
+
+</body>
+</html>
+
